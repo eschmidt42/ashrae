d7eea47e2366f59fe457046ffd3e1752b136d6c1
eschmidt42 <11818904+eschmidt42@users.noreply.github.com>
2020-11-15 16:19:39 +0100

    update

--- set_link.sh
+++ set_link.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-echo 'Linking ashrae/ and nbs/'
 cwd=$(pwd)
+echo 'Linking ashrae/ and nbs/'
 ln -s "${cwd}"/ashrae/ "${cwd}"/nbs/ashrae
+echo 'Linking ashrae/ and other_nbs/'
+ln -s "${cwd}"/ashrae/ "${cwd}"/other_nbs/ashrae
